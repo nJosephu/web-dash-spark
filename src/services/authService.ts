9c0b0b29ae29fa3697d1ca9650d9a4a81dc122f4
nJosephu <nnyamahjosephng@gmail.com>
2025-05-12 08:37:03 +0100

    updated api link

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -24,8 +24,7 @@ interface RegisterResponse {
   };
 }
 
-const API_URL =
-  "https://urgent-2kay-directed-bill-payment-system.onrender.com/api";
+const API_URL = "https://urgent-2kay-directed-bill-payment-system.onrender.com";
 
 const authService = {
   register: async (userData: {
