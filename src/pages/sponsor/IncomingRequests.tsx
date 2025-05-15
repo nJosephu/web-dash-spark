4ec8ff0af38e5f4ad72f39ac7542d9d336a5ec1a
nJosephu <nnyamahjosephng@gmail.com>
2025-05-15 18:33:03 +0100

    ui updatee

--- src/pages/sponsor/IncomingRequests.tsx
+++ src/pages/sponsor/IncomingRequests.tsx
@@ -181,19 +181,6 @@ const SponsorIncomingRequests = () => {
         </Select>
       </div>
 
-      <div className="flex flex-col sm:flex-row gap-3 mb-6">
-        <Button
-          variant="outline"
-          className="w-full sm:w-auto"
-          onClick={() => {
-            setSearchQuery("");
-            setStatusFilter("all");
-          }}
-        >
-          Clear filters
-        </Button>
-      </div>
-
       {filteredRequests.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {filteredRequests.map((request) => (
